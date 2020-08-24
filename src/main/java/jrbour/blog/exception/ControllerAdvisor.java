11f4d282a8c81870d84192875c7d9ca7f1f8cfaa
JrBour <calaruni@hotmail.com>
2020-08-24 18:00:21 +0200

    feat(exception): Add method for handle NotFoundException

--- src/main/java/jrbour/blog/exception/ControllerAdvisor.java
+++ src/main/java/jrbour/blog/exception/ControllerAdvisor.java
@@ -1,4 +1,18 @@
 package jrbour.blog.exception;
 
-public class ControllerAdvisor  {
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ControllerAdvisor  extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    protected ResponseEntity<Object> handleNotFoundException(NotFoundException ex, WebRequest req) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, req);
+    }
 }
