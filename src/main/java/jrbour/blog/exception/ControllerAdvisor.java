1e66deba29b49749530bfeb362f5429441026276
JrBour <calaruni@hotmail.com>
2020-08-30 16:47:05 +0200

    feat(exception): Edit return message when a bad request happen

--- src/main/java/jrbour/blog/exception/ControllerAdvisor.java
+++ src/main/java/jrbour/blog/exception/ControllerAdvisor.java
@@ -1,16 +1,20 @@
 package jrbour.blog.exception;
 
 import org.hibernate.exception.ConstraintViolationException;
-import org.springframework.core.NestedExceptionUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 @ControllerAdvice
@@ -30,6 +34,25 @@ public class ControllerAdvisor  extends ResponseEntityExceptionHandler {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("error", "Constraint violation");
 //        String message = NestedExceptionUtils.getMostSpecificCause(ex).getMessage();
+        return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.BAD_REQUEST, req);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+    MethodArgumentNotValidException ex,
+    HttpHeaders headers,
+    HttpStatus status,
+    WebRequest req) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        List<String> errors = new ArrayList<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.add(error.getField() + " : " + error.getDefaultMessage());
+        }
+        for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
+            errors.add(error.getObjectName() + " : " + error.getDefaultMessage());
+        }
+        body.put("error", errors);
+
         return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.BAD_REQUEST, req);
     }
 }
