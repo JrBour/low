4f3b81d76898f63bd893c58c5e96ecb67a172e29
JrBour <calaruni@hotmail.com>
2020-09-09 18:25:07 +0200

    fix(categoryServiceTest): Fix test that check if the method save from CategoryService works correctly

--- src/test/java/jrbour/blog/service/CategoryServiceTest.java
+++ src/test/java/jrbour/blog/service/CategoryServiceTest.java
@@ -5,30 +5,33 @@ import jrbour.blog.model.Category;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.ArgumentMatchers.any;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class CategoryServiceTest {
 
-    @Autowired
+    @InjectMocks
     private CategoryService categoryService;
 
-    @MockBean
+    @Mock
     private CategoryDao repository;
 
     @Test
     @DisplayName("Test save category")
     void testSave(){
         Category category = new Category("Gender Reveal Party");
-        doReturn(category).when(repository).save(any());
+        doReturn(category).when(this.repository).save(any());
         // when(repository.save(any(Category.class))).thenReturn(new Category());
 
-        Category created = categoryService.save(category);
+        Category created = this.categoryService.save(category);
         Assertions.assertEquals("Gender Reveal Party", created.getName());
     }
 }
