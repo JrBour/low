69cfd7fe40d471aaaa7aa26f066ccabfa69a3804
JrBour <calaruni@hotmail.com>
2020-09-14 20:38:36 +0200

    refactor(categoryServiceTest): Remove useless namespace

--- src/test/java/jrbour/blog/service/CategoryServiceTest.java
+++ src/test/java/jrbour/blog/service/CategoryServiceTest.java
@@ -9,8 +9,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.ArgumentMatchers.any;
